187c0817a9f3807d12f1e6dad3f80775a7128f85
이종섭 <ljs48425707@gmail.com>
2020-06-30 15:44:48 +0900

    ADD_Asset_Other
    Asset 나머지 파일 추가

---
+++ historiana/Assets/Scripts/BulletMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    Transform tr;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 3.0f;
+        tr = GetComponent<Transform>();
+        StartCoroutine(DestroySelf());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.Translate(Vector2.up * speed * Time.deltaTime); //Vector2.up 은 new Vector2(0,1)을 단축한 것, 해당 값에 SPEED를 곱하면 위로 움직인다.
+    }
+
+    IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(5.0f);
+        Destroy(this.gameObject);
+    }
+}
---
+++ historiana/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Transform tr;
+    public float speed;
+    public float speed2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2.0f;
+        tr = GetComponent<Transform>();
+        speed2 = (GameObject.Find("GameManager").GetComponent<Score>().score/100+1);
+        StartCoroutine(DestroySelf());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.Translate(Vector2.down * speed * speed2 * Time.deltaTime); 
+        //Vector2.up 은 new Vector2(0,1)을 단축한 것, 해당 값에 SPEED를 곱하면 위로 움직인다.
+    }
+
+    IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(6.0f);
+        Destroy(this.gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Vector2 gps = new Vector2(tr.position.x, tr.position.y);
+        if (gps.y<3.3)
+        {
+            Destroy(this.gameObject);
+            GameObject.Find("GameManager").GetComponent<Score>().score += 10;
+        }
+        Destroy(collision.gameObject);
+    }
+}
